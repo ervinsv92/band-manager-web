034eae2033a5544d3bd4a76b0d95fc3d1d4ce105
ervinsv92 <ervinsv92@gmail.com>
2021-10-10 18:21:44 -0600

    home navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 export const Navbar = () => {
     return (
         <div>
+            <Link to='/'>Home</Link>
             <Link to='/bio'>Bio</Link>
             <Link to='/discography'>Discography</Link>
             <Link to='/members'>Members</Link>
